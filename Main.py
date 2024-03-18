74e5630828639396f918facbfd0fdb62616e4bda
guru-krithick <145373188+guru-krithick@users.noreply.github.com>
2024-03-18 14:38:56 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ if file:
   #Department wise Distribution
   data = pd.read_csv(file)
 
-  col1, col2, col3 = st.columns([1, 1, 1])
+  col1, gap1, col2, gap2, col3 = st.columns([1, 1, 1, 1, 1])
 
   with col1:
     dept_counts = data['Department'].value_counts()
@@ -22,7 +22,7 @@ if file:
 
     st.bar_chart(dept_df, x='Department', y='Count')
 
-  with col3:
+  with col2:
     #Attended or not
     ranks = data['Rank']
     zero = 0
@@ -39,3 +39,15 @@ if file:
     })
 
     st.bar_chart(presence_df, x='Presence', y='Count')
+
+  with col3:
+    #Problem count
+    problems_count = data['ProbCount'].value_counts()
+    problems = list(problems_count.index)
+
+    problem_df = pd.DataFrame({
+      'Problems' : problems,
+      'Count' : problems_count
+    })
+
+    st.bar_chart(problem_df, x='Problems', y='Count')
