2cc240386f9cb4d4ce6941a6f4612ed6a65622ac
guru-krithick <145373188+guru-krithick@users.noreply.github.com>
2024-03-18 14:45:54 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,10 +9,22 @@ if file:
   #Department wise Distribution
   data = pd.read_csv(file)
 
+  years = ['All'] + list(data['Year'].value_counts().index)
+
+  year_select = st.selectbox('Year', years, index=0)
+
+  if year_select:
+    filtered_data = pd.DataFrame()
+
+    if year_select = 'All':
+      filtered_data = data
+    else:
+      filtered_data = data[data.Year == year_select]
+
   col1, gap1, col2, gap2, col3 = st.columns([1, 0.3, 1, 0.2, 1])
 
   with col1:
-    dept_counts = data['Department'].value_counts()
+    dept_counts = filtered_data['Department'].value_counts()
     dept = list(dept_counts.index)
 
     dept_df = pd.DataFrame({
@@ -24,7 +36,7 @@ if file:
 
   with col2:
     #Attended or not
-    ranks = data['Rank']
+    ranks = filtered_data['Rank']
     zero = 0
 
     for rank in ranks:
@@ -42,7 +54,7 @@ if file:
 
   with col3:
     #Problem count
-    problems_count = data['ProbCount'].value_counts()
+    problems_count = filtered_data['ProbCount'].value_counts()
     problems = list(problems_count.index)
 
     problem_df = pd.DataFrame({
